536c2c6bae5a5eccbd97b80fecd2919fd31d2430
TrungKevin <nguyentrungkien24sg@gmail.com>
2026-03-16 14:07:45 +0700

    Initial commit with CI/CD workflow

---
+++ nguyentrungkien/src/test/java/com/example/nguyentrungkien/NguyentrungkienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nguyentrungkien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyentrungkienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
